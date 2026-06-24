201303a24b4b486c84f3e3628b201a3eb0ae67fc
DEV-ANALISTAJOAO <joaocicerosilva@outlook.com>
2026-06-24 00:32:40 +0000

    ACRESCENTADO DENSIDADE POPULACIONAL E RENDA PER CAPITA NO PROGRAMA DE CADASTRO DE CARTAS SUPER TRUNFO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
@@ -10,22 +8,26 @@ int main() {
   // Variáreis para carta 1:
 
   char estado1; // Uma letra entre A E H (sendo 8 estados com 4 cidades cada estado)
-  char codigo_carta1[4]; // uma letra escolhida e um número de 01 a 04
+  char codigo_carta1[5]; // uma letra escolhida e um número de 01 a 04
   char nome_cidade1[20]; // qualquer cidade (nome separado por '_')
   int populacao1; // número de habitantes da cidade
   float area1; // área da cidade em km²
   float pib1; // pib da cidade escolhida
   int pontos_turisticos1; // quantidade de pontos turisticos da cidade
+  float quociente_densidade_populacional1; // população da cidade dividida pela área da cidade
+  float quociente_pib_percapita1; // pib da cidade dividido pela população da cidade
 
   // Variáreis para carta 2:
 
   char estado2; // Uma letra entre A E H (sendo 8 estados com 4 cidades cada estado)
-  char codigo_carta2[4]; // uma letra escolhida e um número de 01 a 04
+  char codigo_carta2[5]; // uma letra escolhida e um número de 01 a 04
   char nome_cidade2[20]; // qualquer cidade (nome separado por '_')
   int populacao2; // número de habitantes da cidade
   float area2; // área da cidade em km²
   float pib2; // pib da cidade escolhida
   int pontos_turisticos2; // quantidade de pontos turisticos da cidade
+  float quociente_densidade_populacional2; // população da cidade dividida pela área da cidade
+  float quociente_pib_percapita2; // pib da cidade dividido pela população da cidade
 
   // Área para entrada de dados
 
@@ -53,7 +55,7 @@ int main() {
   printf("Digite a área da cidade em km²:\n");
   scanf(" %f", &area1); // definidor de formato %f tipo float para leitura de número com casa decimal
 
-  printf("Digite o PIB da cidade:\n");
+  printf("Digite o PIB da cidade em milhões ou bilhões:\n");
   scanf(" %f", &pib1); // definidor de formato %f tipo float para leitura de número com casa decimal
 
   printf("Digite a quantidade de pontos turísticos da cidade:\n");
@@ -78,12 +80,19 @@ int main() {
   printf("Digite a área da cidade em km²:\n");
   scanf(" %f", &area2); // definidor de formato %f tipo float para leitura de número com casa decimal
 
-  printf("Digite o PIB da cidade:\n");
+  printf("Digite o PIB da cidade em milhões ou bilhões:\n");
   scanf(" %f", &pib2); // definidor de formato %f tipo float para leitura de número com casa decimal
 
   printf("Digite a quantidade de pontos turísticos da cidade:\n");
   scanf(" %d", &pontos_turisticos2); // definidor de formato %d tipo int para leitura de número inteiro
 
+  // Área para cálculos das propriedades das cidades:
+
+  quociente_densidade_populacional1 = (float) populacao1 / area1; // cálculo da densidade populacional da cidade 1
+  quociente_densidade_populacional2 = (float) populacao2 / area2; // cálculo da densidade populacional da cidade 2
+  quociente_pib_percapita1 = (float) (pib1 * 1000000000.00) / populacao1; // cálculo do PIB per capita da cidade 1
+  quociente_pib_percapita2 = (float) (pib2 * 1000000000.00) / populacao2; // cálculo do PIB per capita da cidade 2
+  
   // Área para exibição dos dados da cidade
 
   printf("\nDados das cartas cadastradas:\n");
@@ -95,6 +104,8 @@ int main() {
   printf("Área: %.2f km²\n", area1); // exibição da área da carta 1 com 2 casas decimais
   printf("PIB: %.2f bilhões de reais\n", pib1); // exibição do PIB da carta 1 com 2 casas decimais
   printf("Número de Pontos turísticos: %d\n", pontos_turisticos1); // exibição da quantidade de pontos turísticos da carta 1
+  printf("Densidade Populaional: %.2f hab/km²\n", quociente_densidade_populacional1); // exibição da densidade populacional da carta 1 com 2 casas decimais
+  printf("PIB per capita: %.2f reais\n", quociente_pib_percapita1); // exibição do PIB per capita da carta 1 com 2 casas decimais
 
   printf("\nCarta 2\n"); // cabeçalho para exibição das informações da carta 2
   printf("Estado: %c\n", estado2); // exibição do estado da carta 2
@@ -104,6 +115,8 @@ int main() {
   printf("Área: %.2f km²\n", area2); // exibição da área da carta 2 com 2 casas decimais
   printf("PIB: %.2f bilhões de reais\n", pib2); // exibição do PIB da carta 2 com 2 casas decimais
   printf("Número de Pontos turísticos: %d\n", pontos_turisticos2); // exibição da quantidade de pontos turísticos da carta 2
+  printf("Densidade Populacional: %.2f hab/km²\n", quociente_densidade_populacional2); // exibição da densidade populacional da carta 2 com 2 casas decimais
+  printf("PIB per capita: %.2f reais\n", quociente_pib_percapita2); // exibição do PIB per capita da carta 2 com 2 casas decimais
 
 return 0;
 } 
